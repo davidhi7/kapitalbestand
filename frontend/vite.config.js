64900ed33077ec6098c5057c237479518a5bc528
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-12 20:22:42 +0100

    set vite 'host' to current ip address

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { networkInterfaces } from 'node:os';
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+
+const interfaces = networkInterfaces();
+
+const ipaddr = interfaces.enp8s0[0].address;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: ipaddr,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: false,
+                secure: false
+            }
+        }
     }
-  },
-  server: {
-    // host: '192.168.2.170',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: false,
-        secure: false,
-      }
-    }
-  }
-})
+});
