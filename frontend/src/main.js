62c6366a8d4566ef0ee90eb5f68325bdecbd2d36
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-14 01:57:38 +0100

    Reformat and clean up entrypoint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,36 +1,36 @@
-import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createApp } from 'vue';
 import { Vue3Mq } from 'vue3-mq';
-
-import { eventEmitter } from '@/views/components/Notification.vue';
-import router from '@/router';
-import App from '@/views/App.vue';
-
-import { useAuthStateStore } from '@/stores/AuthStateStore';
-import { useCategoryShopStore } from '@/stores/CategoryShopStore';
-import { useTransactionsStore } from './stores/TransactionsStore';
 
 import '@/assets/base.less';
 import '@/assets/icons.less';
+import router from '@/router';
+import { useAuthStateStore } from '@/stores/AuthStateStore';
+import { useCategoryShopStore } from '@/stores/CategoryShopStore';
+import App from '@/views/App.vue';
+import { eventEmitter } from '@/views/components/Notification.vue';
 
-
+import { useTransactionsStore } from './stores/TransactionsStore';
 
 const app = createApp(App);
 const pinia = createPinia();
+
 app.use(router);
 app.use(pinia);
 app.use(Vue3Mq, {
     preset: 'tailwind'
 });
-app.mount('div#app');
+
 const AuthStateStore = useAuthStateStore();
 const CategoryShopStore = useCategoryShopStore();
 const TransactionsStore = useTransactionsStore();
 
-
-AuthStateStore.requestWhoAmI();
-// TODO: do this on login
-CategoryShopStore.fetch();
-TransactionsStore.fetch();
+AuthStateStore.requestWhoAmI().then(() => {
+    app.mount('div#app');
+    if (AuthStateStore.authenticated) {
+        CategoryShopStore.fetch();
+        TransactionsStore.fetch();
+    }
+});
 
 app.config.globalProperties.$notificationBus = eventEmitter;
