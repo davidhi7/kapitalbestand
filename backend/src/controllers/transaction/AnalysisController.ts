daa2db17e6325f98c20314e1ba2d3d53f1dc6edf
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-03-14 01:55:19 +0100

    Implement basic analysis for transactions

---
+++ backend/src/controllers/transaction/AnalysisController.ts
@@ -0,0 +1,100 @@
+import { Op } from 'sequelize';
+import { QueryTypes } from 'sequelize';
+
+import sequelize, { MonthlyTransaction } from '../../database/db.js';
+
+type MonthlySummary = Array<{
+    oneoff: {
+        expenses: number;
+        incomes: number;
+    };
+    monthly: {
+        expenses: number;
+        incomes: number;
+    };
+}>;
+
+class AnalysisController {
+    /**
+     * Summarise all monthly / one-off expenses by month within the given year.
+     * @param year
+     * @returns
+     */
+    async getYearlyStatistics(year: number): Promise<MonthlySummary> {
+        const monthlyValues: MonthlySummary = Array.from({ length: 12 });
+        for (let i = 0; i < 12; i++) {
+            monthlyValues[i] = {
+                oneoff: {
+                    expenses: 0,
+                    incomes: 0
+                },
+                monthly: {
+                    expenses: 0,
+                    incomes: 0
+                }
+            };
+        }
+        const oneoffTransactionsSummary: Array<{
+            year: number;
+            month: number;
+            expenses: number;
+            incomes: number;
+        }> = await sequelize.query(
+            `
+        SELECT
+            EXTRACT(YEAR FROM "OneoffTransactions"."date") as year,
+            EXTRACT(month FROM "OneoffTransactions"."date") as month,
+            SUM(CASE WHEN "Transactions"."isExpense" = true THEN "Transactions"."amount" else 0 END) as expenses,
+            SUM(CASE WHEN "Transactions"."isExpense" = false THEN "Transactions"."amount" else 0 END) as incomes 
+        FROM
+            "OneoffTransactions", "Transactions"
+        WHERE
+            "Transactions"."id" = "OneoffTransactions"."TransactionId"
+            AND
+            EXTRACT(YEAR FROM "OneoffTransactions"."date") = :year
+        GROUP BY
+            year, month;
+        `,
+            {
+                replacements: {
+                    year
+                },
+                type: QueryTypes.SELECT
+            }
+        );
+        for (let entry of oneoffTransactionsSummary) {
+            monthlyValues[entry.month - 1].oneoff.expenses = Number(entry.expenses);
+            monthlyValues[entry.month - 1].oneoff.incomes = Number(entry.incomes);
+        }
+
+        // doing the same as with one-off transactions but with monthly transactions is a lot more difficult, so now we approach this in a simpler way
+        const matchingMonthlyTransactions = await MonthlyTransaction.findAll({
+            where: {
+                monthFrom: {
+                    [Op.between]: [new Date(year, 0, 1), new Date(year, 11, 31)]
+                },
+                monthTo: {
+                    [Op.or]: {
+                        [Op.eq]: null,
+                        [Op.between]: [new Date(year, 0, 1), new Date(year, 11, 31)]
+                    }
+                }
+            }
+        });
+        for (let monthlyTransaction of matchingMonthlyTransactions) {
+            const month0 = new Date(monthlyTransaction.monthFrom).getMonth();
+            const month1 = monthlyTransaction.monthTo ? new Date(monthlyTransaction.monthTo).getMonth() : 11;
+            for (let i = month0; i <= month1; i++) {
+                if (monthlyTransaction.Transaction.isExpense) {
+                    monthlyValues[i].monthly.expenses += monthlyTransaction.Transaction.amount;
+                } else {
+                    monthlyValues[i].monthly.incomes += monthlyTransaction.Transaction.amount;
+                }
+            }
+        }
+
+        return monthlyValues;
+    }
+}
+
+export default new AnalysisController();
--- backend/src/routing/api.js
+++ backend/src/routing/api.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import createError from 'http-errors';
-import { errorHandler } from './error-handling.js';
+
 import authentication from './authentication.js';
-import transactions from './endpoints/transactions.js';
+import analysis from './endpoints/analysis.js';
 import categories from './endpoints/categories.js';
 import shops from './endpoints/shops.js';
-
+import transactions from './endpoints/transactions.js';
+import { errorHandler } from './error-handling.js';
 
 const router = express.Router();
 router.use(express.json());
@@ -16,6 +16,7 @@ router.use(authentication);
 router.use('/transactions', transactions);
 router.use('/categories', categories);
 router.use('/shops', shops);
+router.use('/analysis', analysis);
 
 router.use(errorHandler);
 
