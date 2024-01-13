adfeddf57dbaeee5112e73eab22292b473eca793
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2024-01-13 02:09:28 +0100

    More work

--- frontend/src/pages/base/components/AutoComplete.vue
+++ frontend/src/pages/base/components/AutoComplete.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { defineModel, defineProps } from 'vue';
 
+import Input from './Input.vue';
+
 const model = defineModel();
 const props = defineProps<{
     suggestions: string[];
@@ -8,12 +10,11 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div>
-        <input class="peer" type="text" :model="model" />
-        <div class="absolute hidden peer-focus-visible:block bg-black">
-            <div class="flex flex-col flex-grow">
-                <button v-for="suggestion in [1, 2, 3, 4, 5]">{{ suggestion }}</button>
-            </div>
+    <!-- TODO outlines -->
+    <div class="relative group focus-within:bg-input-bg dark:focus-within:bg-input-bg-dark">
+        <Input class="outline-none" type="text" :standalone="false" v-model="model" />
+        <div class="absolute w-full z-10 hidden group-focus-within:flex flex-col items-start group-focus-within:bg-input-bg group-focus-within:dark:bg-input-bg-dark">
+            <button class="px-2 m-1" v-for="suggestion in [1, 2, 3, 4, 5]" @click.prevent="model=suggestion">{{ suggestion }}</button>
         </div>
     </div>
 </template>
