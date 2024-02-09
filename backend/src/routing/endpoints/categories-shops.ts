20fdbb05ac8b048e6c90924d035e62cccfe3beb8
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2024-02-09 17:55:04 +0100

    Extract validator

--- backend/src/routing/endpoints/categories-shops.ts
+++ backend/src/routing/endpoints/categories-shops.ts
@@ -1,4 +1,4 @@
-import { body, query } from 'express-validator';
+import { body } from 'express-validator';
 
 import CategoryShopController, { CategoryOrShop } from '../../controllers/category-shop/CategoryShopController.js';
 import { Category, Shop } from '../../database/db.js';
@@ -12,13 +12,13 @@ const nameLengthValidator = body('name').isLength({ min: 1 });
 
 function generateRouterByModel(model: CategoryOrShop) {
     return new EndpointBuilder<CategoryShopParameters, CategoryShopParameters>()
-        .post([body('name').isLength({ min: 1 })], async (user, body) => {
+        .post([nameLengthValidator], async (user, body) => {
             return await CategoryShopController.create(model, user, body.name);
         })
-        .get([query('name').isLength({ min: 1 }).optional()], async (user, limit, offset, { name }) => {
+        .get([nameLengthValidator], async (user, limit, offset, { name }) => {
             return await CategoryShopController.fetch(model, user, limit, offset, name);
         })
-        .patch([body('name').isLength({ min: 1 })], async (user, id, body) => {
+        .patch([nameLengthValidator], async (user, id, body) => {
             return await CategoryShopController.update(model, user, id, body.name);
         })
         .delete(async (user, id) => {
