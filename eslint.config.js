873888faf38d7de90a29ccef7bc199b871ddf9c4
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2024-03-27 22:22:10 +0100

    Fix eslint setup for vuejs

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
 import mochaPlugin from 'eslint-plugin-mocha';
+import vuePlugin from 'eslint-plugin-vue';
+import tseslint from 'typescript-eslint';
+import vueParser from 'vue-eslint-parser';
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  mochaPlugin.configs.flat.recommended,
-  {
-    "rules": {
-      "indent": ["warn", 4],
-      "space-before-function-paren": 'off'
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    mochaPlugin.configs.flat.recommended,
+    ...vuePlugin.configs['flat/recommended'],
+    {
+        rules: {
+            indent: ['warn', 4],
+            'vue/html-indent': ['warn', 4],
+            'space-before-function-paren': 'off'
+        },
+        "languageOptions": {
+            parser: vueParser,
+            parserOptions: {
+                parser: tseslint.parser,
+                sourceType: 'module'
+            }
+        }
     }
-  }
-)
+);
