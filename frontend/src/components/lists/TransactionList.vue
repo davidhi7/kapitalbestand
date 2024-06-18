82f9d163e84d2d6637df8bdd5881fa06a854612b
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2024-06-18 02:13:03 +0200

    New CSS style based logic for hiding columns based on viewport width

--- frontend/src/components/lists/TransactionList.vue
+++ frontend/src/components/lists/TransactionList.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts" generic="T extends OneoffTransaction | MonthlyTransaction">
-import { ref, shallowRef, watch } from 'vue';
+import { computed, ref, shallowRef, watch } from 'vue';
 
 import { MonthlyTransaction, OneoffTransaction } from '@backend-types/TransactionTypes';
+import { breakpointsTailwind } from '@vueuse/core';
 
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import DeleteAction from '@/components/lists/DeleteAction.vue';
@@ -9,8 +10,11 @@ import EditAction from '@/components/lists/EditAction.vue';
 import QuickActionModal from '@/components/lists/QuickActionModal.vue';
 import TransactionListHeader from '@/components/lists/TransactionListHeader.vue';
 import TransactionListRow from '@/components/lists/TransactionListRow.vue';
-import type { Breakpoint, ColumnSettings } from '@/components/lists/listConfig';
+import type { Breakpoint, ColumnSettings, NotEmptyBreakpoint } from '@/components/lists/listConfig';
 import { breakpoints } from '@/components/lists/listConfig';
+
+// Generate a unique CSS identifier for the table, because we may need to apply CSS rules specific to this component instance, without relying on scoped css
+const id = 'id' + Math.floor(Math.random() * 1_000_000).toString();
 
 const props = withDefaults(
     defineProps<{
@@ -41,15 +45,32 @@ function del(transaction: OneoffTransaction | MonthlyTransaction) {
 
 const tailwindColumnRules = ref<string[]>([]);
 
+type ConditionalColumnList<T extends OneoffTransaction | MonthlyTransaction> = {
+    index: number;
+    column: Omit<ColumnSettings<T>, 'breakpoint'> & { breakpoint: NotEmptyBreakpoint };
+}[];
+const conditionalColumns = computed<ConditionalColumnList<T>>(() => {
+    return props.columnSettings
+        .map((column, index) => {
+            return { column, index };
+        })
+        .filter((column) => column.column.breakpoint !== '') as ConditionalColumnList<T>;
+});
+
 watch(
-    props,
+    () => props.columnSettings,
     () => {
-        // Always at least one column for actions except for smallest screen size
+        // Always at least one column for actions (except for smallest screen size) plus N columns for all non-conditional columns
+        const alwaysVisibleColumnCount =
+            props.columnSettings.length - conditionalColumns.value.length;
         const columnsByBreakpoint = Object.fromEntries(
-            breakpoints.map((breakpoint) => [breakpoint, breakpoint ? 1 : 0])
+            breakpoints.map((breakpoint) => [
+                breakpoint,
+                alwaysVisibleColumnCount + (breakpoint ? 1 : 0)
+            ])
         ) as Record<Breakpoint, number>;
 
-        for (const column of props.columnSettings) {
+        for (const { column, index } of conditionalColumns.value) {
             const minBreakpoint = breakpoints.indexOf(column.breakpoint);
             for (let i = minBreakpoint; i < breakpoints.length; i++) {
                 columnsByBreakpoint[breakpoints[i]]++;
@@ -66,7 +87,13 @@ watch(
 </script>
 
 <template>
-    <table class="grid-cols grid w-full" :class="tailwindColumnRules">
+    <!-- `style` tags are unfortunately not allowed in components, so we need to do a hack here -->
+    <component :is="'style'" v-for="{ column, index } in conditionalColumns">
+        @media screen and (max-width:
+        {{ breakpointsTailwind[column.breakpoint] - 1 }}px) { table#{{ id }} > :is(tbody, thead) >
+        tr > :is(td, th):nth-child({{ index + 1 }}) { display: none !important; } }
+    </component>
+    <table :id="id" class="grid-cols grid w-full" :class="tailwindColumnRules">
         <thead class="contents">
             <TransactionListHeader :column-settings="props.columnSettings" />
         </thead>
@@ -87,7 +114,7 @@ watch(
             <span v-if="props.loading">
                 <LoadingSpinner />
             </span>
-            <span v-else> Keine Einträge </span>
+            <span v-else>Keine Einträge</span>
         </tbody>
     </table>
     <QuickActionModal
