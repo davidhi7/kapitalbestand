5dbecfd557b05a00c0d9b2fb2fab95925766f49a
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2024-08-28 17:50:00 +0200

    Remove redundant code

--- backend/src/database/User.test.js
+++ backend/src/database/User.test.js
@@ -4,19 +4,11 @@ import { User } from './db.js';
 
 describe('user model', function () {
     it('should not include the password hash on default query', async function () {
-        User.create({
-            username: 'testuser',
-            hash: 'testhash'
-        });
         const user = await User.findOne();
         expect(user.hash).to.be.undefined;
     });
 
     it('should include the password hash using the `with_hash` scope', async function () {
-        User.create({
-            username: 'testuser',
-            hash: 'testhash'
-        });
         const user = await User.scope('with_hash').findOne();
         expect(user.hash).to.not.be.undefined;
     });
